b52985cf03c6048431ce999344516045fd1405b5
imdark <michael@phantomauto.com>
2021-08-19 08:58:00 -0700

    making the code easier to test against

---
+++ run.sh
@@ -0,0 +1,7 @@
+gst-launch-1.0  videotestsrc num-buffers=30 ! filesink location=out.I420
+gpu_id=0
+input_file=out.I420
+output_file=out.mp4
+width=320
+height=240
+python3 SampleEncode.py $gpu_id $input_file $output_file $width $height
